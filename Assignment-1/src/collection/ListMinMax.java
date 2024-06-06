4121be7ff87d371fa2abdb43022131349aafda84
Deepak Vishwakarma <20eg106248@anurag.edu.in>
2024-06-06 14:56:51 +0530

    daily update

---
+++ Assignment-1/src/collection/ListMinMax.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.List;
+import java.util.Arrays;
+
+import java.util.Collections;
+
+public class ListMinMax {
+	public static void main(String args[]) {
+		List<Integer> num=Arrays.asList(40,39,8,44,10,16,33);
+		int mini=Collections.min(num);
+		int maxi=Collections.max(num);
+		
+		System.out.println("Min= " + mini + " Max= " + maxi);
+	}
+}
